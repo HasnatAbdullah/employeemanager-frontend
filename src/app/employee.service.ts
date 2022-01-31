7dd5e435ec7127cf5605d8917c9814262a3959be
Hasnat Abdullah <45420800+HasnatAbdullah@users.noreply.github.com>
2022-01-31 16:01:31 +0600

    added app.component.ts where the methods reside. THIS shows the stored objects in db with the backend app running

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -8,8 +8,8 @@ import { environment } from 'src/environments/environment';
     providedIn: 'root'
 })
 export class EmployeeService {
-//   private apiServerUrl = 'environment.apiBaseUrl';
-  private apiServerUrl = '';
+  private apiServerUrl = environment.apiBaseUrl;
+
   constructor(private http: HttpClient){}
 
   public getEmployees(): Observable<Employee[]> {
