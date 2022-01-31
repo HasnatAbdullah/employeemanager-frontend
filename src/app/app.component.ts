239a7c79d62c6807c03f561581f67a06615d539c
Hasnat Abdullah <45420800+HasnatAbdullah@users.noreply.github.com>
2022-01-31 17:03:32 +0600

    have shown data in cards format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -88,7 +88,7 @@ export class AppComponent implements OnInit {
     }
   }
 
-  public onOpenModal(employee: Employee, mode: string): void {
+ public onOpenModal(employee: Employee, mode: string): void {
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
     button.type = 'button';
